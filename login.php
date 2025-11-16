647bd49d648525fd38d347563826f264c1a0280a
dndraamy <dndramylza12@gmail.com>
2025-11-16 17:34:41 +0700

    update login admin fix

--- login.php
+++ login.php
@@ -1,3 +1,45 @@
+<?php
+include 'koneksi.php'; 
+session_start();
+
+if (isset($_SESSION['is_admin_login']) && $_SESSION['is_admin_login'] === true) {
+    header('Location: dashboard_admin.php'); 
+    exit;
+}
+
+$error = '';
+
+if (isset($_POST['login_submit'])) {
+    $email = trim($_POST['email']);
+    $password = $_POST['password'];
+    if (empty($email) || empty($password)) {
+        $error = "Email dan password harus diisi.";
+    } else {
+        $stmt = $conn->prepare("SELECT id, email, password, nama FROM admin_users WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows === 1) {
+            $user = $result->fetch_assoc();
+            if ($password === $user['password']) {
+                $_SESSION['is_admin_login'] = true;
+                $_SESSION['admin_id'] = $user['id'];
+                $_SESSION['admin_email'] = $user['email'];
+                $_SESSION['admin_nama'] = $user['nama'];
+                header('Location: dashboard_admin.php'); 
+                exit;
+            } else {
+                $error = "Password salah.";
+            }
+        } else {
+            $error = "Tidak terdaftar sebagai Admin.";
+        }
+        $stmt->close();
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,39 +47,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="styles_css/login.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" />
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <title>Login Admin</title>
+    <title>Login Admin LavaLink</title>
 </head>
 
 <body>
     <div class="container">
         <div class="login">
-            <h1>Login Admin</h1>
-            <form action="">
+            <h1 style="font-size: 21px;"><b>ᨒ Login Admin LavaLink</b></h1>
+            
+            <?php if (!empty($error)): ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $error; ?>
+                </div>
+            <?php endif; ?>
+            
+            <form action="" method="POST">
                 <div class="input-box">
-                    <input type="email" placeholder="Email">
+                    <input type="email" placeholder="Email" name="email" required>
                     <i class="fa fa-envelope"></i>
                 </div>
                 <div class="input-box">
-                    <input type="password" placeholder="Password">
+                    <input type="password" placeholder="Password" name="password" required>
                     <i class="fa fa-lock"></i>
                 </div>
-                <div class="rembar">
-                    <input id="rembar" type="checkbox">
-                    <label for="rembar">remember me</label>
-                </div>
-                <button type="submit">LOGIN</button>
+                <button type="submit" name="login_submit" style="padding-right: 50px">LOGIN</button>
                 <div class="links">
-                    <a href="#">Back to Home</a>
-                </div>
+                    <a href="index.php">Kembali ke Halaman Utama</a> </div>
             </form>
         </div>
     </div>
-
 </body>
 </html>
--- logout.php
+++ logout.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
+session_unset();
 session_destroy();
-header("Location: index.php");
-exit();
+
+if (isset($_COOKIE['user_remember'])) {
+    setcookie('user_remember', '', time() - 3600, "/");
+}
+
+header('Location: index.php');
+exit;
 ?>
